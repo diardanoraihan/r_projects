0afca20d6df4b4a87e9a52d7841f8e342ed74870
Diardano Raihan <diardano@gmail.com>
2022-07-17 08:00:52 +0700

    feat: add markdown and kable/qplot visualization

---
+++ [Reminder]/1. Data Visualization in R with Tidyverse/8_r_markdown_intro.R
@@ -0,0 +1,6 @@
+# install.packages("tinytex")
+# tinytex::install_tinytex() # install tinytex
+
+# Creating a report using R Markdown
+# R markdown is a file that contains text like you might find in a word processing document but it also incorporates R code and visualizations. 
+
